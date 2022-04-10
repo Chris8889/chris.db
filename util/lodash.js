d690e87f10fd7d4f44f8a5aab00bf6e32755eb0f
Chris <67835702+Chris8889@users.noreply.github.com>
2022-04-10 12:33:49 +0100

    Update lodash.js

--- util/lodash.js
+++ util/lodash.js
@@ -3,7 +3,7 @@ const lodash_get = require(`lodash.get`);
 const lodash_has = require(`lodash.has`);
 const lodash_unset = require(`lodash.unset`);
 
-exports.get = lodash_get.default;
-exports.has = lodash_has.default;
-exports.set = lodash_set.default;
-exports.unset = lodash_unset.default;
+exports.get = lodash_get
+exports.has = lodash_has
+exports.set = lodash_set
+exports.unset = lodash_unset
